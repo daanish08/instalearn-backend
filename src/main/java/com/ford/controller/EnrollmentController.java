acc1ed74d4f7613629931cab208d747d691f1377
Suriya T <rhythmsuriya@gmail.com>
2024-11-14 12:58:26 +0530

    Updated

--- src/main/java/com/ford/controller/EnrollmentController.java
+++ src/main/java/com/ford/controller/EnrollmentController.java
@@ -1,18 +1,13 @@
 package com.ford.controller;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ford.dto.EnrollmentDTO;
-import com.ford.entity.Admin;
-import com.ford.entity.Course;
 import com.ford.entity.Enrollment;
 import com.ford.service.CourseServiceImpl;
 import com.ford.service.EnrollmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -67,7 +62,7 @@ public class EnrollmentController {
         System.out.println(userId);
 
         //errrororoororor
-        List<Enrollment> data= (List<Enrollment>) enrollmentService.findPendingEnrollmentsByAdminId(userId);
+        List<Enrollment> data= (List<Enrollment>) enrollmentService.findEnrollmentsByUserId(userId);
         System.out.println(data);
         return ResponseEntity.ok(data);
     }
